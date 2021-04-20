b9c2d446058b488fe458b572e1e1c7c305a44b01
AlbertoPMDM <albertopmdm@gmail.com>
2021-04-20 13:13:50 -0500

    Added elevator subsystem

---
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -0,0 +1,38 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.Ultrasonic;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.ElevatorConstants;
+
+public class Elevator extends SubsystemBase{
+
+    public static SpeedController m_elevator = new WPI_TalonSRX(ElevatorConstants.TALONSRX_ELEVATOR);
+
+    public Ultrasonic m_elevatorSensor = new Ultrasonic(
+        ElevatorConstants.ULTRASONIC_ELEVATOR[0], 
+        ElevatorConstants.ULTRASONIC_ELEVATOR[1]);
+
+    public Elevator(){
+        m_elevatorSensor.setAutomaticMode(true);
+    }
+
+    public double readSensor(){
+        return m_elevatorSensor.getRangeMM();
+    }
+
+    public void up(){
+        if (readSensor()>10.0){
+            m_elevator.set(0.5);
+        }
+        else{
+            m_elevator.set(0.5);
+        }
+    }
+
+    public void down(){
+        m_elevator.set(-0.5);
+    }
+}
